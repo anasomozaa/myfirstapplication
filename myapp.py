bdce798c303a9f106623160acea0ab79c01b9620
anasomozaa <52749830+anasomozaa@users.noreply.github.com>
2024-04-22 19:10:05 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -16,14 +16,14 @@ st.image(image, caption='Logo KDT')
 
 st.title('My first web app')
 
-@st.cache
-def display_dataframe():
-  df = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-  return(df)
+#@st.cache
+#def display_dataframe():
+df = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+  #return(df)
 
 
-dataframe1 = display_dataframe()
-st.write(dataframe1)
+#dataframe1 = display_dataframe()
+st.write(df)
  
 
 #option = st.selectbox('Choose an option', ['a', 'b', 'c'])
